28d515ff6c91c6431976756e9b5f26260a4d65d3
heroadn <benbenjamin554@gmail.com>
2021-06-09 15:57:52 -0300

    aplied coding style

--- src/cpu.c
+++ src/cpu.c
@@ -8,39 +8,130 @@ typedef struct cpu_type
 {
     //architecture
     uint8_t v[15], delay, timer;
-    uint16_t stack[16], pc, sp, i;
-
-    Instructions *instructions[16];
+    uint16_t stack[16], pc, sp, i;;
 };
 
 CPU cpu_create()
 {
     CPU cpu = malloc(sizeof(struct cpu_type));
-
-    cpu->instructions[0] = nop;
-    cpu->instructions[1] = _1nnn;
-    cpu->instructions[2] = _2nnn;
-    cpu->instructions[3] = _3xkk;
-    cpu->instructions[4] = not_implmented;
-    cpu->instructions[5] = not_implmented;
-    cpu->instructions[6] = _6xkk;
-    cpu->instructions[7] = not_implmented;
-    cpu->instructions[8] = not_implmented;
-    cpu->instructions[9] = not_implmented;
-    cpu->instructions[10] = _annn;
-    cpu->instructions[11] = not_implmented;
-    cpu->instructions[12] = not_implmented;
-    cpu->instructions[13] = _dxyn;
-    cpu->instructions[14] = not_implmented;
-    cpu->instructions[15] = not_implmented;
-
+    cpu->sp = 0;
     return cpu;
 }
 
-Instructions* cpu_get_instruction(CPU cpu,
-                          uint16_t instr)
+Instruction *const cpu_get_instruction(CPU cpu,
+                                        uint16_t op)
 {
-    return cpu->instructions[instr >> 12];
+    switch (op & 0xF000)
+    {
+    case 0x1000:
+        return _1nnn;
+        break;
+    case 0x2000:
+        return _2nnn;
+        break;
+    case 0x3000:
+        return _3xkk;
+        break;
+    case 0x4000:
+        return _4xkk;
+        break;
+    case 0x5000:
+        return _5xy0;
+        break;
+    case 0x6000:
+        return _6xkk;
+        break;
+    case 0x7000:
+        return _7xkk;
+        break;
+    case 0x9000:
+        return _9xy0;
+        break;
+    case 0xD000:
+        return _dxyn;
+        break;
+    case 0xA000:
+        return _annn;
+        break;
+    default:
+
+        switch (op & 0xF0FF)
+        {
+        case 0x0000:
+            return nop;
+            break;
+        case 0x00E0:
+            return not_implmented;
+            break;
+        case 0x00EE:
+            return _00ee;
+            break;
+        }
+
+        switch (op & 0xF00F)
+        {
+        case 0x8000:
+            return _8xy0;
+            break;
+        case 0x8001:
+            return _8xy1;
+            break;
+        case 0x8002:
+            return not_implmented;
+            break;
+        case 0x8003:
+            return not_implmented;
+            break;
+        case 0x8004:
+            return not_implmented;
+            break;
+        case 0x8005:
+            return not_implmented;
+            break;
+        case 0x8006:
+            return not_implmented;
+            break;
+        case 0x8007:
+            return not_implmented;
+            break;
+        case 0x800E:
+            return not_implmented;
+            break;
+        }
+
+        switch (op & 0xF0FF)
+        {
+        case 0xF007:
+            return not_implmented;
+            break;
+        case 0xF00A:
+            return not_implmented;
+            break;
+        case 0xF015:
+            return not_implmented;
+            break;
+        case 0xF018:
+            return not_implmented;
+            break;
+        case 0xF01E:
+            return not_implmented;
+            break;
+        case 0xF029:
+            return not_implmented;
+            break;
+        case 0xF033:
+            return not_implmented;
+            break;
+        case 0xF055:
+            return not_implmented;
+            break;
+        case 0xF065:
+            return not_implmented;
+            break;
+        }
+
+        return not_implmented;
+    }
 }
 
 void cpu_destroy(CPU cpu)
@@ -56,6 +147,21 @@ void cpu_inc_pc_byte(CPU cpu)
 void cpu_inc_pc_word(CPU cpu)
 {
     cpu_set_pc(cpu, cpu_get_pc(cpu) + 2);
+}
+
+void cpu_or_reg(CPU cpu,
+                uint8_t src,
+                uint8_t dst)
+{
+    cpu_set_reg(cpu, dst, cpu_get_reg(cpu, src) | cpu_get_reg(cpu, dst));
+}
+
+void cpu_ld_reg(CPU cpu,
+                uint8_t src,
+                uint8_t dst
+                )
+{
+    cpu_set_reg(cpu, dst, cpu_get_reg(cpu, src));
 }
 
 void cpu_set_pc(CPU cpu,
@@ -113,4 +219,28 @@ uint16_t cpu_get_reg_I(CPU cpu)
 uint16_t cpu_get_pc(CPU cpu)
 {
     return cpu->pc;
+}
+
+uint16_t cpu_get_sp(CPU cpu)
+{
+    return cpu->sp;
+}
+
+uint16_t cpu_stack_pop(CPU cpu)
+{
+    return cpu->stack[--cpu->sp];
+}
+
+void cpu_stack_push(CPU cpu,
+                    uint16_t value)
+{
+    cpu->stack[cpu->sp++] = value;
+}
+
+void cpu_stack_print(CPU cpu)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        printf("    %2d: %x\n", i, cpu->stack[i]);
+    }
 }
--- src/cpu.h
+++ src/cpu.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
 
 typedef struct cpu_type *CPU;
-typedef void (*Instructions)(CPU cpu, void *, void *, uint16_t op);
+typedef void (*Instruction)(CPU cpu,
+                             void *,
+                             void *,
+                             uint16_t op);
 
 CPU cpu_create();
 
@@ -10,6 +13,14 @@ void cpu_destroy();
 void cpu_inc_pc_byte(CPU cpu);
 
 void cpu_inc_pc_word(CPU cpu);
+
+void cpu_ld_reg(CPU cpu,
+                uint8_t src,
+                uint8_t dst);
+
+void cpu_or_reg(CPU cpu,
+                uint8_t src,
+                uint8_t dst);                
 
 void cpu_set_pc(CPU cpu,
                 uint16_t pc);
@@ -27,8 +38,8 @@ void cpu_set_reg_timer(CPU cpu,
 void cpu_set_reg_I(CPU cpu,
                    uint16_t value);
 
-Instructions* cpu_get_instruction(CPU cpu,
-                          uint16_t instr);
+Instruction *cpu_get_instruction(CPU cpu,
+                                  uint16_t instr);
 
 uint8_t cpu_get_reg(CPU cpu,
                     uint8_t n);
@@ -40,3 +51,12 @@ uint8_t cpu_get_reg_timer(CPU cpu);
 uint16_t cpu_get_reg_I(CPU cpu);
 
 uint16_t cpu_get_pc(CPU cpu);
+
+uint16_t cpu_get_sp(CPU cpu);
+
+uint16_t cpu_stack_pop(CPU cpu);
+
+void cpu_stack_push(CPU cpu,
+                    uint16_t value);
+
+void cpu_stack_print(CPU cpu);
